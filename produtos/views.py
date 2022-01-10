da4c00701646ad089ebddb8d89465b3a24820e97
PiedroCraft <88720549+PiedroCraft@users.noreply.github.com>
2022-01-10 01:19:15 -0300

    update3

--- produtos/views.py
+++ produtos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Produto
 from .form import ProdutoForm
+import os
 
 
 @login_required
@@ -23,8 +24,13 @@ def produto_new(reguest):
 def produto_update(reguest, id):
     produto = get_object_or_404(Produto, pk=id)
     form = ProdutoForm(reguest.POST or None, reguest.FILES or None, instance=produto)
+    foto_old = None
+    if produto.foto:
+        foto_old = produto.foto
 
     if form.is_valid():
+        if foto_old:
+            os.remove(f"media/{foto_old}")
         form.save()
         return redirect("produto_list")
 
@@ -33,8 +39,13 @@ def produto_update(reguest, id):
 @login_required
 def produto_delete(reguest, id):
     produto = get_object_or_404(Produto, pk=id)
+    foto_old = None
+    if produto.foto:
+        foto_old = produto.foto
 
     if reguest.method == "POST":
+        if foto_old:
+            os.remove(f"media/{produto.foto}")
         produto.delete()
         return redirect("produto_list")
 
